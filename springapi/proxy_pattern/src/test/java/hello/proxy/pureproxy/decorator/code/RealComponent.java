d265f77bad1d76cf77e9e2a056524e16d7783691
yeham kim <dev.yamkim@gmail.com>
2024-01-01 21:43:10 +0900

    feat: [springacvanced] 타임 데코레이터 관련 간단한 예시 추가

--- springapi/proxy_pattern/src/test/java/hello/proxy/pureproxy/decorator/code/RealComponent.java
+++ springapi/proxy_pattern/src/test/java/hello/proxy/pureproxy/decorator/code/RealComponent.java
@@ -3,7 +3,7 @@ package hello.proxy.pureproxy.decorator.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class RealComponent implements Component{
+public class RealComponent implements Component {
 
     @Override
     public String operation() {
